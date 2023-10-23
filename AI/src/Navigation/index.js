49fbd9cffb9769a4dcfc80730b011c021dfc005c
Nagendra07-moh <nagedra.mohan.5@gmail.com>
2023-10-23 11:33:19 +0530

    .

--- AI/src/Navigation/index.js
+++ AI/src/Navigation/index.js
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen   from '../Screens/HomeScreen'
@@ -11,7 +10,7 @@ const Stack = createNativeStackNavigator();
 function AppNavigation() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName='WelcomeScreen' screenOptions={{headerShown:false}}>
         <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} />
         <Stack.Screen name="HomeScreen" component={HomeScreen} />
       </Stack.Navigator>
--- AI/src/Screens/WelcomeScreen.js
+++ AI/src/Screens/WelcomeScreen.js
@@ -1,10 +1,23 @@
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { View, Text } from 'react-native';
+import { SafeAreaView, TouchableOpacity } from 'react-native';
+import { View, Text,Image } from 'react-native';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 
 export default function WelcomeScreen() {
+  const navigation = useNavigation();
   return (
-    <View>
-      <Text>Welcome Here!!</Text>
-     </View>
+    <SafeAreaView className="flex-1 flex justify-around bg-white ">
+      <View className="space-y-2">
+        <Text style={{fontSize:wp(10)}} className="text-center font-bold text-gray-600 ">Jarvis </Text>
+        <Text style={{fontSize:wp(4)}} className="text-center tracking-wide font-semibold ">The future is Here, powered by AI</Text>
+      </View>
+      <View className="flex-row justify-center">
+        <Image source={require('../../assets/Images/welcome.png')} style={{width:wp(75),height:wp(75)}} />
+      </View>
+      <TouchableOpacity onPress={()=> navigation.navigate('HomeScreen')} className="bg-emerald-600 mx-5 p-4 rounded-2xl">
+        <Text style={{fontSize:wp(6)}} className="text-white font-bold text-center">Get Started</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 }
