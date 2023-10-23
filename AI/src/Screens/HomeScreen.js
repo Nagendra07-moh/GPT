d935f67fa9cebcbf12bf9283819ec3ab0e7125da
Nagendra07-moh <nagedra.mohan.5@gmail.com>
2023-10-23 12:04:48 +0530

    .

--- AI/src/Screens/HomeScreen.js
+++ AI/src/Screens/HomeScreen.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, SafeAreaView,Image } from 'react-native';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>Home Here!!</Text>
-     </View>
+    <View className ="flex-1  bg-white">
+    <SafeAreaView className="flex-1 flex mx-5">
+    {/*bot icon*/}
+    <View className="flex-row justify-center">
+      <Image source={require('../../assets/Images/bot.png')} style={{height:hp(15) ,width:hp(15)}} className="mt-4"/>
+    </View>
+
+    </SafeAreaView>
+    </View>
   );
 }
